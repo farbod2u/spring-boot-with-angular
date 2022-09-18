44cbc5cf0da2940e4b2d7bba0a96b143c0552422
farbod2u <saeedsaf@gmail.com>
2022-09-18 18:12:28 +0430

    CrossOrigin

--- back-end/src/main/java/ir/saeed/backend/controller/EmployeeController.java
+++ back-end/src/main/java/ir/saeed/backend/controller/EmployeeController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/employees")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
